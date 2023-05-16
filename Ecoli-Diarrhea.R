3191752bcfc9c2fbea92f694b3d70304e169dd74
smnhasan <nayeem5847@gmail.com>
2023-05-16 23:55:52 +0600

    title added

--- Ecoli-Diarrhea.R
+++ Ecoli-Diarrhea.R
@@ -15,6 +15,7 @@ require(maptools)
 require(dplyr)
 
 ##############Only For Cross Tab and COR########
+
 ## importing datasheet
 setwd('E:\\Update - Ecoli\\Bangladesh MICS6 SPSS Datasets')
 wm <- as.data.frame(read.spss('wm.sav',use.value.labels=F),stringsAsFactors = FALSE)
@@ -28,6 +29,7 @@ merdat2 <- left_join(x = merdat, y = hh, by=c("HH1", "HH2"))
 str(merdat2)
 merdat$stratum.x
 
+
 ## subsetting datasets
 f <- c('CA1','WQ26','WQ27','WQ12','WS1','CAGE_11','HL4','HH6.x','HH7.x',
        'melevel','windex5.x','HC1A','HHSEX','ethnicity','WAGE','WS11',
@@ -38,6 +40,8 @@ f <- c('CA1','WQ26','WQ27','WQ12','WS1','CAGE_11','HL4','HH6.x','HH7.x',
 
 fd <- merdat2[,f]
 
+
+#Surveyweight
 #design1 <- svydesign(id=fd$HH1, strata = fd$stratum,   weights=fd$chweight,data=fd)
 design1 <- svydesign(id=fd$HH1,  weights=fd$chweight, data=fd)
 
@@ -49,6 +53,7 @@ factor(fd$diarrhea)
 fd$diarrhea <- factor(fd$diarrhea,levels=c(1,2),labels = c('yes','no'))
 factor(fd$diarrhea)
 
+
 #household water test (100ml).
 factor(fd$WQ26)
 fd$WQ26 <- ifelse(fd$WQ26==991|fd$WQ26==992 ,NA,fd$WQ26)
@@ -59,13 +64,8 @@ fd$hhwq <- ifelse(fd$WQ26==0,0,ifelse(fd$WQ26==1|fd$WQ26==2|fd$WQ26==3|
 factor(fd$hhwq)
 fd$hhwq <- factor(fd$hhwq,levels=c(0,1,2),labels = c('Low','Moderate','High'))
 factor(fd$hhwq)
-
-
 fd <- subset(fd, !is.na(fd$hhwq))
-# View(fd)
-# nrow(fd)
 fd <- subset(fd, !is.na(fd$diarrhea))
-
 round(svytable(~fd$hhwq+fd$diarrhea,design=design1))
 round(svytable(~fd$hhwq,design=design1))
 prop.table(svytable(~fd$hhwq,design=design1))*100
@@ -83,7 +83,6 @@ fd$swq <- ifelse(fd$WQ27==0,0,ifelse(fd$WQ27==1|fd$WQ27==2|fd$WQ27==3|
 factor(fd$swq)
 fd$swq <- factor(fd$swq,levels=c(0,1,2),labels = c('Low','Moderate','High'))
 factor(fd$swq)
-
 round(svytable(~fd$swq+fd$diarrhea,design=design1))
 round(svytable(~fd$swq,design=design1))
 prop.table(svytable(~fd$swq,design=design1))*100
@@ -98,15 +97,11 @@ fd$sw <- ifelse(fd$WQ12==1, 1, ifelse(fd$WQ12==2,2,3))
 factor(fd$sw)
 fd$sw <- factor(fd$sw,levels=c(1,2,3),labels = c('Direct from source','Covered container','Uncovered container'))
 factor(fd$sw)
-
 round(svytable(~fd$sw+fd$diarrhea,design=design1))
 round(svytable(~fd$swq,design=design1))
 prop.table(svytable(~fd$swq,design=design1))*100
 prop.table(svytable(~fd$swq+fd$diarrhea,design=design1), margin=1)*100
 #svychisq(~fd$hhwq+fd$diarrhea,design=design1, data=fd)  #sig
-
-
-
 
 
 #Source water type (100ml).
@@ -117,7 +112,6 @@ fd$swt <- ifelse(fd$WS1==11|fd$WS1==12|fd$WS1==13|fd$WS1==14|fd$WS1==21|
 factor(fd$swt)
 fd$swt <- factor(fd$swt,levels=c(1,2),labels = c('Improved','Unimproved'))
 factor(fd$swt)
-
 round(svytable(~fd$swt+fd$diarrhea,design=design1))
 round(svytable(~fd$swt,design=design1))
 prop.table(svytable(~fd$swt,design=design1))*100
@@ -127,52 +121,51 @@ prop.table(svytable(~fd$swt+fd$diarrhea,design=design1), margin=1)*100
 
 #Child Age
 fd$cage <- factor(fd$CAGE_11)
-
 round(svytable(~fd$cage+fd$diarrhea,design=design1))
 round(svytable(~fd$cage,design=design1))
 prop.table(svytable(~fd$cage,design=design1))*100
 prop.table(svytable(~fd$cage+fd$diarrhea,design=design1), margin=1)*100
 #svychisq(~fd$hhwq+fd$diarrhea,design=design1, data=fd)  #sig
 
+
 #Child sex
 fd$csex <- fd$HL4
 fd$csex <- factor(fd$csex,levels=c(1,2),labels = c('Male','Female'))
 factor(fd$csex)
-
 round(svytable(~fd$csex+fd$diarrhea,design=design1))
 round(svytable(~fd$csex,design=design1))
 prop.table(svytable(~fd$csex,design=design1))*100
 prop.table(svytable(~fd$csex+fd$diarrhea,design=design1), margin=1)*100
 #svychisq(~fd$hhwq+fd$diarrhea,design=design1, data=fd)  #sig
 
+
 #Residence.
 fd$residence <- fd$HH6.x
 fd$residence <- factor(fd$HH6.x,levels=c(1,2,3,4,5),labels = c('Rural','Urban','Urban','Urban','Rural'))
 factor(fd$residence)
-
 round(svytable(~fd$residence+fd$diarrhea,design=design1))
 round(svytable(~fd$residence,design=design1))
 prop.table(svytable(~fd$residence,design=design1))*100
 prop.table(svytable(~fd$residence+fd$diarrhea,design=design1), margin=1)*100
 #svychisq(~fd$hhwq+fd$diarrhea,design=design1, data=fd)  #sig
 
+
 #Division.
 fd$division <- fd$HH7.x
 fd$division <- factor(fd$HH7.x,levels=c(10,20,30,40,45,50,55,60),labels = c("Barisal","Chattogram","Dhaka","Khulna","Mymensingh","Rajshahi","Rangpur","Sylhet"))
 factor(fd$HH7.x)
-
 round(svytable(~fd$HH7.x+fd$diarrhea,design=design1))
 round(svytable(~fd$HH7.x,design=design1))
 prop.table(svytable(~fd$HH7.x,design=design1))*100
 prop.table(svytable(~fd$HH7.x+fd$diarrhea,design=design1), margin=1)*100
 #svychisq(~fd$hhwq+fd$diarrhea,design=design1, data=fd)  #sig
 
+
 #Education.
 factor(fd$melevel)
 fd$melevel <- ifelse(fd$melevel==9,NA,fd$melevel)
 fd$mel <- factor(fd$melevel,levels=c(0,1,2,3),labels = c('P','LS','SH','H'))
 factor(fd$mel)
-
 round(svytable(~fd$mel+fd$diarrhea,design=design1))
 round(svytable(~fd$mel,design=design1))
 prop.table(svytable(~fd$mel,design=design1))*100
@@ -186,15 +179,14 @@ fd$windex <- ifelse(fd$windex5.x==0,NA,fd$windex5.x)
 factor(fd$windex)
 fd$windex <- factor(fd$windex,levels=c(1,2,3,4,5),labels = c('Poor','Poor','Middle','Rich','Rich'))
 factor(fd$windex)
-
 round(svytable(~fd$windex+fd$diarrhea,design=design1))
 round(svytable(~fd$windex,design=design1))
 prop.table(svytable(~fd$windex,design=design1))*100
 prop.table(svytable(~fd$windex+fd$diarrhea,design=design1), margin=1)*100
 #svychisq(~fd$hhwq+fd$diarrhea,design=design1, data=fd)  #sig
 
-#Religion.
 
+#Religion.
 # fd$religion <- ifelse(fd$HC1A==9,NA,fd$HC1A)
 # factor(fd$religion)
 # fd$religion <- factor(fd$religion,levels=c(1,2,3,4),labels = c('Islam','Others','Others','Others'))
@@ -205,7 +197,6 @@ prop.table(svytable(~fd$windex+fd$diarrhea,design=design1), margin=1)*100
 # prop.table(svytable(~fd$religion,design=design1))*100
 # prop.table(svytable(~fd$religion+fd$diarrhea,design=design1), margin=1)*100
 # #svychisq(~fd$hhwq+fd$diarrhea,design=design1, data=fd)  #sig
-
 
 
 #household sex.
@@ -225,11 +216,11 @@ prop.table(svytable(~fd$windex+fd$diarrhea,design=design1), margin=1)*100
 fd$tf <- ifelse(fd$WS11==99,NA,fd$WS11)
 fd$tf <- ifelse(fd$WS11<24,1,2)
 fd$tf <- factor(fd$tf,levels=c(1,2),labels = c('Improved','Unimproved'))
-
 round(svytable(~fd$tf+fd$diarrhea,design=design1))
 round(svytable(~fd$tf,design=design1))
 prop.table(svytable(~fd$tf,design=design1))*100
 prop.table(svytable(~fd$tf+fd$diarrhea,design=design1), margin=1)*100
+
 
 #Salt Iodization.
 fd$si <- ifelse(fd$SA1==9,NA,fd$SA1)
@@ -253,7 +244,6 @@ factor(fd$si)
 fd$animal <- ifelse(fd$HC17==9,NA,fd$HC17)
 fd$animal <- factor(fd$animal,levels=c(1,2),labels = c('Yes','No'))
 factor(fd$animal)
-
 round(svytable(~fd$animal+fd$diarrhea,design=design1))
 round(svytable(~fd$animal,design=design1))
 prop.table(svytable(~fd$animal,design=design1))*100
@@ -265,62 +255,56 @@ median(fd$HH48)
 fd$hhmem <- ifelse(fd$HH48<5,1,2)
 fd$hhmem <- factor(fd$hhmem,levels=c(1,2),labels = c('Small','Large'))
 factor(fd$hhmem)
-
 round(svytable(~fd$hhmem+fd$diarrhea,design=design1))
 round(svytable(~fd$hhmem,design=design1))
 prop.table(svytable(~fd$hhmem,design=design1))*100
 prop.table(svytable(~fd$hhmem+fd$diarrhea,design=design1), margin=1)*100
 
+
 #Toilet facility shared
 fd$tfshared <- ifelse(fd$WS15==9,NA,fd$WS15)
 fd$tfshared <- factor(fd$tfshared,levels=c(1,2),labels = c('Yes','No'))
 factor(fd$tfshared)
-
 round(svytable(~fd$tfshared+fd$diarrhea,design=design1))
 round(svytable(~fd$tfshared,design=design1))
 prop.table(svytable(~fd$tfshared,design=design1))*100
 prop.table(svytable(~fd$tfshared+fd$diarrhea,design=design1), margin=1)*100
+
 
 #Treat water to make safer for drinking
 fd$watertreat <- ifelse(fd$WS9==8|fd$WS9==9,NA,fd$WS9)
 fd$watertreat <- factor(fd$watertreat,levels=c(1,2),labels = c('Yes','No'))
 factor(fd$watertreat)
-
 round(svytable(~fd$tfshared+fd$diarrhea,design=design1))
 round(svytable(~fd$tfshared,design=design1))
 prop.table(svytable(~fd$tfshared,design=design1))*100
 prop.table(svytable(~fd$tfshared+fd$diarrhea,design=design1), margin=1)*100
 
 
+#Removing missing values
 str(fd)
 fd <- subset(fd, !is.na(diarrhea))
 fd <- subset(fd, !is.na(hhwq))
 str(fd)
 
-
-
+#Crude model (Logistic regression model)
 logit1 <- (svyglm(relevel(factor(diarrhea), ref = "no") ~  csex, family=quasibinomial(link=logit), data=fd, design=design1 , na.action = na.omit, rescale=TRUE))
 summary(logit1) #sig
-
 model.or <- cbind(exp(coef(logit1)),exp(confint(logit1)))
 colnames(model.or) <- c('odds ratio','ci(2.5%)','ci(97.5%)')
 model.or #for odds ratio and ci
 
-
 logit1 <- (svyglm(relevel(factor(diarrhea), ref = "no") ~ factor(cage), family=quasibinomial(link=logit), data=fd, design=design1 , na.action = na.omit, rescale=TRUE))
 summary(logit1) #sig
-
 model.or <- cbind(exp(coef(logit1)),exp(confint(logit1)))
 colnames(model.or) <- c('odds ratio','ci(2.5%)','ci(97.5%)')
 model.or #for odds ratio and ci
 
 logit1 <- (svyglm(relevel(factor(diarrhea), ref = "no") ~  factor(mel), family=quasibinomial(link=logit), data=fd, design=design1 , na.action = na.omit, rescale=TRUE))
 summary(logit1) #sig
-
 model.or <- cbind(exp(coef(logit1)),exp(confint(logit1)))
 colnames(model.or) <- c('odds ratio','ci(2.5%)','ci(97.5%)')
 model.or #for odds ratio and ci
-
 
 logit1 <- (svyglm(relevel(factor(diarrhea), ref = "no") ~ hhmem, family=quasibinomial(link=logit), data=fd, design=design1 , na.action = na.omit, rescale=TRUE))
 summary(logit1) #sig
@@ -331,29 +315,24 @@ model.or #for odds ratio and ci
 
 logit1 <- (svyglm(relevel(factor(diarrhea), ref = "no") ~    animal, family=quasibinomial(link=logit), data=fd, design=design1 , na.action = na.omit, rescale=TRUE))
 summary(logit1) #sig
-
 model.or <- cbind(exp(coef(logit1)),exp(confint(logit1)))
 colnames(model.or) <- c('odds ratio','ci(2.5%)','ci(97.5%)')
 model.or #for odds ratio and ci
 
 logit1 <- (svyglm(relevel(factor(diarrhea), ref = "no") ~  windex, family=quasibinomial(link=logit), data=fd, design=design1 , na.action = na.omit, rescale=TRUE))
 summary(logit1) #sig
-
 model.or <- cbind(exp(coef(logit1)),exp(confint(logit1)))
 colnames(model.or) <- c('odds ratio','ci(2.5%)','ci(97.5%)')
 model.or #for odds ratio and ci
 
 logit1 <- (svyglm(relevel(factor(diarrhea), ref = "no") ~ swt, family=quasibinomial(link=logit), data=fd, design=design1 , na.action = na.omit, rescale=TRUE))
 summary(logit1) #sig
-
 model.or <- cbind(exp(coef(logit1)),exp(confint(logit1)))
 colnames(model.or) <- c('odds ratio','ci(2.5%)','ci(97.5%)')
 model.or #for odds ratio and ci
 
-
 logit1 <- (svyglm(relevel(factor(diarrhea), ref = "no") ~   tf, family=quasibinomial(link=logit), data=fd, design=design1 , na.action = na.omit, rescale=TRUE))
 summary(logit1) #sig
-
 model.or <- cbind(exp(coef(logit1)),exp(confint(logit1)))
 colnames(model.or) <- c('odds ratio','ci(2.5%)','ci(97.5%)')
 model.or #for odds ratio and ci
@@ -361,110 +340,80 @@ model.or #for odds ratio and ci
 
 logit1 <- (svyglm(relevel(factor(diarrhea), ref = "no") ~  tfshared, family=quasibinomial(link=logit), data=fd, design=design1 , na.action = na.omit, rescale=TRUE))
 summary(logit1) #sig
-
 model.or <- cbind(exp(coef(logit1)),exp(confint(logit1)))
 colnames(model.or) <- c('odds ratio','ci(2.5%)','ci(97.5%)')
 model.or #for odds ratio and ci
 
-
 logit1 <- (svyglm(relevel(factor(diarrhea), ref = "no") ~ hhwq, family=quasibinomial(link=logit), data=fd, design=design1 , na.action = na.omit, rescale=TRUE))
 summary(logit1) #sig
-
 model.or <- cbind(exp(coef(logit1)),exp(confint(logit1)))
 colnames(model.or) <- c('odds ratio','ci(2.5%)','ci(97.5%)')
 model.or #for odds ratio and ci
 
 logit1 <- (svyglm(relevel(factor(diarrhea), ref = "no") ~   sw, family=quasibinomial(link=logit), data=fd, design=design1 , na.action = na.omit, rescale=TRUE))
 summary(logit1) #sig
-
 model.or <- cbind(exp(coef(logit1)),exp(confint(logit1)))
 colnames(model.or) <- c('odds ratio','ci(2.5%)','ci(97.5%)')
 model.or #for odds ratio and ci
 
 logit1 <- (svyglm(relevel(factor(diarrhea), ref = "no") ~ swq, family=quasibinomial(link=logit), data=fd, design=design1 , na.action = na.omit, rescale=TRUE))
 summary(logit1) #sig
-
 model.or <- cbind(exp(coef(logit1)),exp(confint(logit1)))
 colnames(model.or) <- c('odds ratio','ci(2.5%)','ci(97.5%)')
 model.or #for odds ratio and ci
 
 logit1 <- (svyglm(relevel(factor(diarrhea), ref = "no") ~  watertreat, family=quasibinomial(link=logit), data=fd, design=design1 , na.action = na.omit, rescale=TRUE))
 summary(logit1) #sig
-
 model.or <- cbind(exp(coef(logit1)),exp(confint(logit1)))
 colnames(model.or) <- c('odds ratio','ci(2.5%)','ci(97.5%)')
 model.or #for odds ratio and ci
 
-
 logit1 <- (svyglm(relevel(factor(diarrhea), ref = "no") ~  residence, family=quasibinomial(link=logit), data=fd, design=design1 , na.action = na.omit, rescale=TRUE))
 summary(logit1) #sig
-
 model.or <- cbind(exp(coef(logit1)),exp(confint(logit1)))
 colnames(model.or) <- c('odds ratio','ci(2.5%)','ci(97.5%)')
 model.or #for odds ratio and ci
 
 logit1 <- (svyglm(relevel(factor(diarrhea), ref = "no") ~  relevel(factor(division), ref = "Sylhet"), family=quasibinomial(link=logit), data=fd, design=design1 , na.action = na.omit, rescale=TRUE))
 summary(logit1) #sig
-
 model.or <- cbind(exp(coef(logit1)),exp(confint(logit1)))
 colnames(model.or) <- c('odds ratio','ci(2.5%)','ci(97.5%)')
 model.or #for odds ratio and ci
 
+#Adjusted model (Logistic regression model)
 #design1 <- svydesign(id=fd$HH1, strata = fd$stratum,   weights=fd$chweight,data=fd)
 design1 <- svydesign(id=fd$HH1,  weights=fd$chweight,data=fd)
-
 
 logit1 <- (svyglm(relevel(factor(diarrhea), ref = "no") ~ cage + csex + mel + hhmem +  animal + windex  + swt  + 
                     tf   + tfshared + hhwq + sw + swq + watertreat + residence + relevel(factor(division), ref = "Sylhet"), 
                   family=quasibinomial(link=logit), data=fd, design=design1, na.action = na.omit))
 summary(logit1) #sig
-
 model.or <- cbind(exp(coef(logit1)),exp(confint(logit1)))
 colnames(model.or) <- c('odds ratio','ci(2.5%)','ci(97.5%)')
 round(model.or,2) #for odds ratio and ci
 
+
 #################Only for AOR and Love plot#########################
 
 ## importing dataset
-
 setwd('E:\\Update - Ecoli\\Bangladesh MICS6 SPSS Datasets')
-
 wm <- as.data.frame(read.spss('wm.sav',use.value.labels=F),stringsAsFactors = FALSE)
-
 wm<- wm[with(wm, order(HH1, HH2, LN)),]
-
 hh <- as.data.frame(read.spss('hh.sav',use.value.labels=F),stringsAsFactors = FALSE)
-
 hh<- hh[with(hh, order(HH1, HH2)),]
-
 ch <- as.data.frame(read.spss('ch.sav',use.value.labels=F),stringsAsFactors = FALSE)
-
 ch<- ch[with(ch, order(HH1, HH2, LN)),]
-
 merdat <- left_join(x = ch, y = wm, by=c("HH1", "HH2","LN"))
-
 merdat2 <- left_join(x = merdat, y = hh, by=c("HH1", "HH2"))
 str(merdat2)
 merdat$melevel
-# x1 <- c(1,1,2,2,3,3)
-# x2 <- c(2,3,1,3,1,2)
-# x <- data.frame(x1,x2)
-# x
-# 
-# y1 <- c(1,2,3)
-# y2 <- c(100,150,130)
-# y <- data.frame(y1,y2)
-# 
-# xy <- merge(x,y,by.x="x2",by.y="y1")
 
 ## subsetting datasets
-
 f <- c('CA1','WQ26','WQ27','WQ12', 'WS1',  'WS15', 'CAGE_11','HL4','HH6.x','HH7.x',
        'melevel','windex5.x','HC1A','HHSEX','ethnicity','WAGE','WS11',
        'EC3A','SA1','MT1','MT2','MT3','WAZ2',
        'HAZ2','WHZ2','HC17','HH48','HC15','HC14','WS15',
        'WS3','WS9','chweight','HH1','stratum',"melevel")
-
 fd <- merdat2[,f]
 fd$WAGE
 design1 <- svydesign(id=fd$HH1, weights=fd$chweight,data=fd)
